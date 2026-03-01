baa41b4d8514a09431dded1d29abf0f79516e268
Emily <Emily@LAPTOP-LAV7O6FF>
2026-03-01 17:18:32 +0200

    Init Project Folder

---
+++ DND_Outcome_Calculator/src/dnd_outcome_calculator/DND_Outcome_Calculator.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dnd_outcome_calculator;
+
+/**
+ *
+ * @author Emilia Skye Hills - HLLMIC031 - University of Cape Town
+ */
+public class DND_Outcome_Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
